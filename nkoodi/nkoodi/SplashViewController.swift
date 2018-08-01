60443697e653ec46d215cbc5f3834c6c81afaec6
Shimaa Magdi <shimaa.magdi@samesystem.com>
2018-08-02 00:02:36 +0300

    update main

---
+++ nkoodi/nkoodi/SplashViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SplashViewController.swift
+//  nkoodi
+//
+//  Created by Shimaa Magdi on 8/1/18.
+//  Copyright © 2018 Hajj Hackathon. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+}
