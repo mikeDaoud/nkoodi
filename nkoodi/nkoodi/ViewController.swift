5ecdea515410c9c874b6ba735ab3c4befd127d01
marko nazmy <marko.naeem@vodafone.com>
2018-08-01 22:23:05 +0300

    BATabBarController integration

--- nkoodi/nkoodi/ViewController.swift
+++ nkoodi/nkoodi/ViewController.swift
@@ -7,12 +7,57 @@
 //
 
 import UIKit
+import BATabBarController
+
 
 class ViewController: UIViewController {
 
+    var baTabBarController: BATabBarController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let vc1 = UIViewController()
+        let vc2 = UIViewController()
+        let vc3 = UIViewController()
+        let vc4 = UIViewController()
+        let vc5 = UIViewController()
+        vc1.view.backgroundColor = UIColor(hex: 0x00c6c35)
+        vc2.view.backgroundColor = UIColor(hex: 0x00c6c70)
+        vc3.view.backgroundColor = UIColor(hex: 0x00c6c90)
+        vc4.view.backgroundColor = UIColor(hex: 0x00c6c80)
+        vc5.view.backgroundColor = UIColor(hex: 0x00c6c10)
+        
+        let option1 = NSMutableAttributedString(string: "QR")
+        option1.addAttribute(.foregroundColor, value: UIColor(hex: 0x757575), range: NSRange(location: 0, length: option1.length))
+        option1.addAttribute(.font, value: UIFont.init(name: "Acens", size: 12), range: NSRange(location: 0, length: option1.length))
+        let tabBarItem = BATabBarItem(image: UIImage(named: "ico_01")?.withRenderingMode(.alwaysTemplate).maskWithColor(color: UIColor(hex: 0x757575)), selectedImage: UIImage(named: "ico_01")?.withRenderingMode(.alwaysTemplate).maskWithColor(color: UIColor(hex: 0x00c6c35)), title: option1)
+        let option2 = NSMutableAttributedString(string: "Balance")
+        option2.addAttribute(.font, value: UIFont.init(name: "Acens", size: 12), range: NSRange(location: 0, length: option2.length))
+        option2.addAttribute(.foregroundColor, value: UIColor(hex: 0x757575), range: NSRange(location: 0, length: option2.length))
+        let tabBarItem2 = BATabBarItem(image: UIImage(named: "ico_02")?.withRenderingMode(.alwaysTemplate).maskWithColor(color: UIColor(hex: 0x757575)), selectedImage: UIImage(named: "ico_02")?.withRenderingMode(.alwaysTemplate).maskWithColor(color: UIColor(hex: 0x00c6c35)), title: option2)
+        let option3 = NSMutableAttributedString(string: "Transfer")
+        option3.addAttribute(.font, value: UIFont.init(name: "Acens", size: 12), range: NSRange(location: 0, length: option3.length))
+        option3.addAttribute(.foregroundColor, value: UIColor(hex: 0x757575), range: NSRange(location: 0, length: option3.length))
+        let tabBarItem3 = BATabBarItem(image: UIImage(named: "ico_03")?.withRenderingMode(.alwaysTemplate).maskWithColor(color: UIColor(hex: 0x757575)), selectedImage: UIImage(named: "ico_03")?.withRenderingMode(.alwaysTemplate).maskWithColor(color: UIColor(hex: 0x00c6c35)), title: option3)
+        let option4 = NSMutableAttributedString(string: "Stores")
+        option4.addAttribute(.font, value: UIFont.init(name: "Acens", size: 12), range: NSRange(location: 0, length: option4.length))
+        option4.addAttribute(.foregroundColor, value: UIColor(hex: 0x757575), range: NSRange(location: 0, length: option4.length))
+        let tabBarItem4 = BATabBarItem(image: UIImage(named: "ico_04")?.withRenderingMode(.alwaysTemplate).maskWithColor(color: UIColor(hex: 0x757575)), selectedImage: UIImage(named: "ico_04")?.withRenderingMode(.alwaysTemplate).maskWithColor(color: UIColor(hex: 0x00c6c35)), title: option4)
+        let option5 = NSMutableAttributedString(string: "More")
+        option5.addAttribute(.font, value: UIFont.init(name: "Acens", size: 12), range: NSRange(location: 0, length: option5.length))
+        option5.addAttribute(.foregroundColor, value: UIColor(hex: 0x757575), range: NSRange(location: 0, length: option5.length))
+        let tabBarItem5 = BATabBarItem(image: UIImage(named: "ico_05")?.withRenderingMode(.alwaysTemplate).maskWithColor(color: UIColor(hex: 0x757575)), selectedImage: UIImage(named: "ico_05")?.withRenderingMode(.alwaysTemplate).maskWithColor(color: UIColor(hex: 0x00c6c35)), title: option5)
+        baTabBarController = BATabBarController()
+        baTabBarController.tabBarItemStrokeColor = UIColor(hex: 0x00c6c35)
+        baTabBarController.viewControllers = [vc1, vc2, vc3, vc4, vc5]
+        baTabBarController.tabBarItems = [tabBarItem ?? BATabBarItem(), tabBarItem2 ?? BATabBarItem(), tabBarItem3 ?? BATabBarItem(), tabBarItem4 ?? BATabBarItem(), tabBarItem5 ?? BATabBarItem()]
+        baTabBarController.setSelectedView(vc1, animated: false)
+        baTabBarController.delegate = self
+        baTabBarController.tabBarBackgroundColor = UIColor.white
+        view.addSubview(baTabBarController.view)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +67,45 @@ class ViewController: UIViewController {
 
 
 }
+
+
+extension ViewController: BATabBarControllerDelegate{
+    func tabBarController(_ tabBarController: BATabBarController!, didSelect viewController: UIViewController!) {
+        
+    }
+    
+    
+}
+
+extension UIImage {
+    func maskWithColor(color: UIColor) -> UIImage? {
+        let maskImage = cgImage!
+        
+        let width = size.width
+        let height = size.height
+        let bounds = CGRect(x: 0, y: 0, width: width, height: height)
+        
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+        let context = CGContext(data: nil, width: Int(width), height: Int(height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
+        
+        context.clip(to: bounds, mask: maskImage)
+        context.setFillColor(color.cgColor)
+        context.fill(bounds)
+        
+        if let cgImage = context.makeImage() {
+            let coloredImage = UIImage(cgImage: cgImage)
+            return coloredImage
+        } else {
+            return nil
+        }
+    }
+}
+
+//class NkoodiFont: UIFont {
+//    func getNkoodiFont(size: CGFont) -> UIFont {
+//        self.
+//        return self.withSize(size)
+//    }
+//}
 
