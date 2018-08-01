e2effbd0e3743993b57790929dcc509851eee164
Shimaa Magdi <shimaa.magdi@samesystem.com>
2018-08-01 23:45:42 +0300

    Add chart

--- nkoodi/nkoodi/BalanceViewController.swift
+++ nkoodi/nkoodi/BalanceViewController.swift
@@ -7,6 +7,37 @@
 //
 
 import UIKit
+import Charts
 
 class BalanceViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var arr = [ChartDataEntry]()
+        var yy: Double = 10
+        var minus = true
+        for i in 0..<10{
+            let y: Double
+            if minus { y = yy - Double(i) } else { y = yy + Double(i) }
+            arr.append(ChartDataEntry(x: Double(i), y: y))
+            minus = !minus
+        }
+        
+        var lineChart = LineChartDataSet(values: arr, label: nil)
+        lineChart.colors = [NSUIColor.clear]
+        lineChart.fillColor = UIColor.red
+        lineChart.mode = .cubicBezier
+        lineChart.drawCirclesEnabled = false
+        lineChart.drawFilledEnabled = true
+        lineChart.lineCapType = .round
+        lineChart.formLineWidth = 0
+        lineChart.drawValuesEnabled = true
+        
+        
+        let data = LineChartData(dataSet: lineChart)
+        chartView.data = data
+        chartView.chartDescription?.text = nil
+    }
 }
+
