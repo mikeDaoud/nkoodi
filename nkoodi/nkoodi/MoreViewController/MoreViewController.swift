35e040aaa7efceba33a8f5d8e2a864fe6b7602be
Shimaa Magdi <shimaa.magdi@samesystem.com>
2018-08-02 23:29:19 +0300

    Adding settings

--- nkoodi/nkoodi/MoreViewController/MoreViewController.swift
+++ nkoodi/nkoodi/MoreViewController/MoreViewController.swift
@@ -8,14 +8,41 @@
 
 import UIKit
 
-class MoreViewController: UIViewController {
-
-    @IBAction func logout(_ sender: Any) {
-        DataStore.shared.logoutCurrentUser()
-    }
+class MoreViewController: BaseViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var settings = ["Change Langauge", "Log Out"]
     
     static func create() -> UIViewController{
         return UIStoryboard(name: "MoreView", bundle: nil).instantiateInitialViewController()!
+    }
+}
+
+extension MoreViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settings.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell", for: indexPath) as! SettingCell
+        cell.config(title: settings[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 1:
+            DataStore.shared.logoutCurrentUser()
+            break
+        default: break
+            
+        }
     }
     
 }
